2030e12e36c567569106240aeb62cad215eac38a
OleksandKur <136488587+OleksandrKur@users.noreply.github.com>
2024-03-21 11:24:57 +0200

    Solved 1-4 assignments

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, repetitionCount) {
+    if(repetitionCount<0){
+        return "ERROR"
+    }
+    let outputString = "";
+    for (let i = 0; i < repetitionCount; i++){
+        outputString = outputString + string;
+    }
+    return outputString;
 };
 
 // Do not edit below this line
