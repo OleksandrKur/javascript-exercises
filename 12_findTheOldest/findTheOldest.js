0fa811d5448c58a1927c7c801938584188f3831f
OleksandrKur <136488587+OleksandrKur@users.noreply.github.com>
2024-04-01 08:47:33 +0300

    solved 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
-    
+const findTheOldest = function(peopleArray) {
+    let oldestPerson;
+    let age = 0
+    for (let person of peopleArray){
+        if("yearOfDeath" in person){
+            if (age < person.yearOfDeath - person.yearOfBirth){
+                age = person.yearOfDeath - person.yearOfBirth;
+                oldestPerson = JSON.parse(JSON.stringify(person));
+            }
+        }
+        else{
+            let date = new Date();
+            let year = date.getFullYear();
+            if (age < year - person.yearOfBirth){
+                age = year - person.yearOfBirth;
+                oldestPerson = JSON.parse(JSON.stringify(person));
+            }
+        }
+    }
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
